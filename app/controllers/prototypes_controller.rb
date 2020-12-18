1b88b4cffd834f574a823b3e2b1ebadb9ea0aaa7
sumitaryosuke <f104193@gmail.com>
2020-12-18 16:20:50 +0900

    redirect 修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -26,7 +26,7 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    unless user_signed_in?
+    unless user_signed_in? 
       redirect_to action: :index
     end
     @prototype = Prototype.find(params[:id])
@@ -35,7 +35,7 @@ class PrototypesController < ApplicationController
   def update
     prototype = Prototype.find(params[:id])
     if prototype.update(prototype_params)
-      redirect_to root_path
+      redirect_to edit_prototype_path
     else
       render :edit
     end
